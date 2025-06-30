7a54241b52d06039c9e02bf38fd5a6df91c57849
mnb3000 <misha.mykolyshyn@gmail.com>
2025-06-30 06:16:11 +0300

    feat(e): add shortened redirect page

---
+++ src/routes/r/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	// let { children } = $props();
+	const urlParams = new URLSearchParams(window.location.search);
+	const url = new URL(
+		`https://app.nocodb.com/#/wb0hm07s/pdlmw2lpk8wiaa3/m9g67kaiilcuzah/vwkri0jpnkbxytft?rowId=${urlParams.get('n')}`
+	);
+
+	window.location.assign(url);
+</script>
+
+<h2
+	class="m-5 scroll-m-20 border-b text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+>
+	Redirecting...
+</h2>
